34478f19db2194fbe7abc9ea7e226bfa595e4103
Dellatettehklu <della.tettehklu@yahoo.com>
2016-03-28 10:40:44 -0500

    changes

--- index.js
+++
@@ -1,7 +0,0 @@
-//$(document).mousemove(function (){
-    //if($(window).height()- event.pageY < 150){
-       // $(".navbar").css("top", "0");
-   // }else{
-     // $(".navbar").css("top", "-400");
-    //}
-//});
